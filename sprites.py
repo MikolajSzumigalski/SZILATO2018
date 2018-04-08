436f48ee9bfa2a7a40d7a9a251e761f3558fd2ca
MikolajBalcerek <mpbalcerek+github@gmail.com>
2018-04-08 22:48:18 +0200

    "animacja" dostania damage

--- sprites.py
+++ sprites.py
@@ -10,7 +10,7 @@ img_folder = os.path.join(game_folder, "img")
 
 class Character(pg.sprite.Sprite, metaclass=ABCMeta):
     """ this is a general character abstract class that provides basis of drawing any character on screen"""
-    def __init__(self, game, x, y, hp, at, deff, lev, exp):
+    def __init__(self, game, x, y, hp, at, deff, lev, exp, max_hp=0):
         self.groups = game.all_sprites
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
@@ -23,6 +23,9 @@ class Character(pg.sprite.Sprite, metaclass=ABCMeta):
         self.deff = deff # obrona
         self.lev = lev # poziom
         self.exp = exp;
+        self.max_hp = max_hp;
+        if (self.max_hp == 0):
+            self.max_hp = self.hp
 
     def move(self, dx=0, dy=0):
         self.x += dx
@@ -37,12 +40,33 @@ class Character(pg.sprite.Sprite, metaclass=ABCMeta):
         self.x = new_x
         self.y = new_y
 
+    def take_damage(self, damage):
+        '''
+        this function handles taking raw number of damage to the character and plays damage animation
+        :param damage:
+        :return:
+        '''
+        self.hp -= damage
+        current_health_percentage = self.hp / self.max_hp
+        if(current_health_percentage > 0):
+            new_size = int(32 * current_health_percentage)
+            self.image = pg.transform.scale(self.image, (new_size, new_size))
+
+
     def update(self):
         self.rect.x = self.x * TILESIZE
         self.rect.y = self.y * TILESIZE
 
+    def add_exp(self, exp):
+        self.exp += exp;
+        #TODO
+
     @abstractmethod
     def die(self):
+        pass
+
+    @abstractmethod
+    def level_up(self):
         pass
 
 class Player(Character):
@@ -64,6 +88,10 @@ class Player(Character):
         print("GAMEOVER")
         program_logic.gameover()
 
+    def level_up(self):
+        #TODO LEVEL UP
+        pass
+
 
 class Monster(Character, metaclass=ABCMeta):
     """Abstract class that provides implementation of Character class, that handles displaying a Monster on screen"""
@@ -79,6 +107,9 @@ class Monster(Character, metaclass=ABCMeta):
         self.hp = 0
         pg.sprite.Sprite.remove(self, self.groups)
         del self
+
+    def level_up(self):
+        pass
 
 class Mglak(Monster):
     def __init__(self, game, x, y):
