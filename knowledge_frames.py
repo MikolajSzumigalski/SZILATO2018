4310097df5b0c9b312189efa6773d042639caf22
MikolajBalcerek <mpbalcerek+github@gmail.com>
2018-06-05 22:27:56 +0200

    tylko dane o Tiles do ram

--- knowledge_frames.py
+++ knowledge_frames.py
@@ -7,20 +7,21 @@ import simplejson
 def save_data(LogicEngine, file_name="frames.json"):
     """this function saves game data in JSON form to the file"""
 
-    monsters_list = LogicEngine.monsters
-    final_monsters_dict = {"MONSTERS": monsters_list}
-
-    player = LogicEngine.player
-    final_player_dict = {"PLAYER": player}
-
-    map_tiles = LogicEngine.map.tiles_data
+    # monsters_list = LogicEngine.monsters
+    # final_monsters_dict = {"MONSTERS": monsters_list}
+    #
+    # player = LogicEngine.player
+    # final_player_dict = {"PLAYER": player}
+    #
+    # map_tiles = LogicEngine.map.tiles_data
     final_tiles_dict = {"TILES": map_tiles}
 
-    mixtures_list = LogicEngine.mixtures
-    final_mixtures_dict = {"MIXTURES" : mixtures_list}
+    # mixtures_list = LogicEngine.mixtures
+    # final_mixtures_dict = {"MIXTURES" : mixtures_list}
 
     jsonpickle.set_encoder_options('simplejson', sort_keys=True, indent=4)
-    final_JSON = jsonpickle.encode([final_player_dict, final_monsters_dict, final_tiles_dict, final_mixtures_dict], unpicklable=False)
+    #final_JSON = jsonpickle.encode([final_player_dict, final_monsters_dict, final_tiles_dict, final_mixtures_dict], unpicklable=False)
+    final_JSON = jsonpickle.encode([final_tiles_dict], unpicklable=False)
     __save_to_file__(final_JSON, file_name)
 
 def __save_to_file__(JSON, file_name, newFile=True):
