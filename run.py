0a8b3bf4380ac61d500b1d95d571161ffd7b7ce0
MikolajBalcerek <mpbalcerek+github@gmail.com>
2018-06-05 22:12:41 +0200

    czysty run.py

--- run.py
+++ run.py
@@ -1,10 +1,5 @@
 #THIS FILE IS RESPONSIBLE FOR RUNNING THE PROGRAM, RUN THIS TO RUN THE APPLICATION
 import program_logic
-import GeneticAlgorithm
-from deap import creator, base
-
 if __name__ == "__main__":
-    # set_creator(creator)
-    # creator.create("FitnessMulti", base.Fitness, weights=(-100.0, 1.0, -10.0))
-    # creator.create("Individual", list, fitness=creator.FitnessMulti)
+
     program_logic.startup()
