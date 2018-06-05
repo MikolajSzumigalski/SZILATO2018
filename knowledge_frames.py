6be3865d813eeef9377fa123e3d006e385e6eed3
MikolajBalcerek <mpbalcerek+github@gmail.com>
2018-06-05 23:35:36 +0200

    Folder na raporty

--- knowledge_frames.py
+++ knowledge_frames.py
@@ -2,6 +2,7 @@ import game
 import jsonpickle
 import copy
 import simplejson
+import re
 #this is a helper file that manages saving the in-game data in JSON SI frame format
 
 def save_data(LogicEngine, file_name="frames.json"):
@@ -22,6 +23,8 @@ def save_data(LogicEngine, file_name="frames.json"):
     jsonpickle.set_encoder_options('simplejson', sort_keys=True, indent=4)
     #final_JSON = jsonpickle.encode([final_player_dict, final_monsters_dict, final_tiles_dict, final_mixtures_dict], unpicklable=False)
     final_JSON = jsonpickle.encode([final_tiles_dict], unpicklable=False)
+    re.sub("logic", "gono", final_JSON, re.MULTILINE)
+    print(final_JSON)
     __save_to_file__(final_JSON, file_name)
 
 def __save_to_file__(JSON, file_name, newFile=True):
