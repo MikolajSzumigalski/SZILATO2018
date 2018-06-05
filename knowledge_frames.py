85779ec05dbb2d8f9c0eaeb0165191d812537de4
Mikołaj Balcerek <mpbalcerek+github@gmail.com>
2018-06-05 22:55:54 +0200

    Ramy tylko z tiles z characteroccupiedby (#27)
    * tylko dane o Tiles do ram
    
    * Map tiles now pickle with characterOccupied

--- knowledge_frames.py
+++ knowledge_frames.py
@@ -7,20 +7,21 @@ import simplejson
 def save_data(LogicEngine, file_name="frames.json"):
     """this function saves game data in JSON form to the file"""
 
-    monsters_list = LogicEngine.monsters
-    final_monsters_dict = {"MONSTERS": monsters_list}
-
-    player = LogicEngine.player
-    final_player_dict = {"PLAYER": player}
+    # monsters_list = LogicEngine.monsters
+    # final_monsters_dict = {"MONSTERS": monsters_list}
+    #
+    # player = LogicEngine.player
+    # final_player_dict = {"PLAYER": player}
 
     map_tiles = LogicEngine.map.tiles_data
     final_tiles_dict = {"TILES": map_tiles}
 
-    mixtures_list = LogicEngine.mixtures
-    final_mixtures_dict = {"MIXTURES" : mixtures_list}
+    # mixtures_list = LogicEngine.mixtures
+    # final_mixtures_dict = {"MIXTURES" : mixtures_list}
 
     jsonpickle.set_encoder_options('simplejson', sort_keys=True, indent=4)
-    final_JSON = jsonpickle.encode([final_player_dict, final_monsters_dict, final_tiles_dict, final_mixtures_dict], unpicklable=False)
+    #final_JSON = jsonpickle.encode([final_player_dict, final_monsters_dict, final_tiles_dict, final_mixtures_dict], unpicklable=False)
+    final_JSON = jsonpickle.encode([final_tiles_dict], unpicklable=False)
     __save_to_file__(final_JSON, file_name)
 
 def __save_to_file__(JSON, file_name, newFile=True):
@@ -31,14 +32,3 @@ def __save_to_file__(JSON, file_name, newFile=True):
         mode = 'a+'
     with open(str(file_name), mode) as f:
         f.writelines(JSON)
-
-#THIS template method will jonsify its class
-    # def toJSON(self):
-    #     """
-    #     This function JSONifies the class
-    #     :return JSON form of class:
-    #     """
-    #     jsonpickle.set_encoder_options('simplejson', sort_keys=True, indent=4)
-    #     json_form = jsonpickle.encode(self, unpicklable=False)
-    #     return json_form
-
